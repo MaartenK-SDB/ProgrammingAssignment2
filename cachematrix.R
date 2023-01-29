d956f08a2e9f41f098cec0fa3bed9b55afddd4a8
Jane Doe <maarten.kleemans@seederdeboer.nl>
2023-01-29 17:24:26 +0100

    Submission for review

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## The following functions take matrixes and inverts them, saving matrix and the inverse matrix (iMatrix) to cache.
+## The structure follows similar logic to the example mean function
+## makeCacheMatrix takes an input matrix, and sets and retrieves the base matrix
+## setiMatrix and getiMatrix are used by cacheSolve to calculate and retrieve previous iMatrixes
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <<- NULL
+  set <- function(y){ #Sets a matrix, technically redundant due to initial call but useful for documentation
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x #retrieves the base matrix
+  setiMatrix <- function(iMatrix) m <<- iMatrix #sets the iMatrix, gets called in cacheSolve
+  getiMatrix <- function() m #retrieves the cached iMatrix
+  list(set = set, get = get,
+       setiMatrix = setiMatrix, 
+       getiMatrix = getiMatrix) #list is used to set the function, allowing for x$calls
 }
 
 
-## Write a short comment describing this function
+## cacheSolve takes a makeCacheMatrix object as input, checks the cache, 
+## and if empty calculates the iMatrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getiMatrix() #Retrieves iMatrix in cache
+  #if no iMatrix present, the next part gets skipped
+  if(!is.null(m)){
+      message("getting cached iMatrix")
+      return(m) #
+  }
+  basematrix <- x$get() #Retrieves matrix
+  m <- solve(basematrix, ...) #Calculates inverseMatrix
+  x$setiMatrix() #Sets the iMatrix to cache
+  m
 }
+
+
+
+
